dc9da0587cefd293348f988b261d076914a17bbd
craigy90 <craigy90@gmail.com>
2021-08-29 18:59:54 +1200

    Initial files

---
+++ README.md
@@ -0,0 +1,6 @@
+Midi Drum
+
+Drum has piezo sensors guled beneath skin. Signal is fed to Pi Pico analog inputs.
+
+Current:
+One piezo at A0
---
+++ code.py
@@ -0,0 +1,35 @@
+import board
+import digitalio
+import pwmio
+import time
+import usb_midi
+import adafruit_midi
+import analogio
+
+from adafruit_midi.note_on import NoteOn
+from adafruit_midi.note_off import NoteOff
+from adafruit_midi.pitch_bend import PitchBend
+from adafruit_midi.control_change import ControlChange
+
+midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
+
+print("MacroPad MIDI Board")
+adc = analogio.AnalogIn(board.A0)
+
+while True:
+
+    raw = adc.value
+    pad1 = int(raw/20)-43
+    if pad1 < 0:
+        pad1 = 0
+    print(pad1)
+    if pad1:
+        midi.send([NoteOn(40, pad1)])
+        print("Note on...")
+        time.sleep(1)
+        midi.send([NoteOff(40, pad1)])
+        print("Note off...")
+        time.sleep(1)
+
+
+    time.sleep(0.01)
