438a2111b337a84d56b0656d2eacfbd24e250035
craigy90 <craigy90@gmail.com>
2021-08-30 21:06:23 +1200

    3 sensors, bug report

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Midi Drum
 Drum has piezo sensors guled beneath skin. Signal is fed to Pi Pico analog inputs.
 
 Current:
-One piezo at A0
+Three piezo sensors partially calibrated
+
+Bug:
+Sometimes all 3 output zeros
--- code.py
+++ code.py
@@ -14,24 +14,65 @@ from adafruit_midi.control_change import ControlChange
 midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
 
 print("MacroPad MIDI Board")
-adc = analogio.AnalogIn(board.A0)
+adc0 = analogio.AnalogIn(board.A0)
+adc1 = analogio.AnalogIn(board.A1)
+adc2 = analogio.AnalogIn(board.A2)
 
 while True:
 
-    raw = adc.value
-    pad1 = int(raw/20)-22
+    raw0 = adc0.value
+    raw1 = adc1.value
+    raw2 = adc2.value
+
+    pad0 = int(raw0/30)-50
+    pad1 = int(raw1/65)-32
+    pad2 = int(raw2/20)-50
+
+    if pad0 < 0:
+        pad0 = 0
+    if pad0 > 127:
+        pad0 = 127
+
     if pad1 < 0:
         pad1 = 0
     if pad1 > 127:
         pad1 = 127
-    print(pad1)
+
+    if pad2 < 0:
+        pad2 = 0
+    if pad2 > 127:
+        pad2 = 127
+
+    if pad0 or pad1 or pad2:
+        time.sleep(0.1)
+        if pad1 > pad0 or pad1 > pad2:
+            pad0 = 0
+            pad2 = 0
+        elif pad2 > pad1 or pad2 > pad0:
+            pad1 = 0
+            pad2 = 0
+        elif pad0 > pad2 or pad0 > pad1:
+            pad2 = 0
+            pad1 = 0
+        print(pad0, "\t", pad1, "\t", pad2)
+
+    if pad0:
+        midi.send([NoteOn(40, pad0)])
+        print("pad0")
+        time.sleep(0.01)
+        midi.send([NoteOff(40, pad0)])
+
     if pad1:
-        midi.send([NoteOn(40, pad1)])
-        print("Note on...")
-        time.sleep(1)
-        midi.send([NoteOff(40, pad1)])
-        print("Note off...")
-        time.sleep(1)
+        midi.send([NoteOn(45, pad1)])
+        print("pad1")
+        time.sleep(0.01)
+        midi.send([NoteOff(45, pad1)])
+
+    if pad2:
+        midi.send([NoteOn(50, pad2)])
+        print("pad2")
+        time.sleep(0.01)
+        midi.send([NoteOff(50, pad2)])
 
 
     time.sleep(0.01)
