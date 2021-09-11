90757f2021ab7779a29676932b5a4967cb7745e9
craigy90 <craigy90@gmail.com>
2021-09-11 22:10:00 +1200

    Clunky - Dah di di dah dah

--- code.py
+++ code.py
@@ -24,13 +24,13 @@ def getMidiInput():
     raw1 = adc1.value
     raw2 = adc2.value
 
-    pad[0] = int(raw0/60)-10
-    pad[1] = int(raw1/120)-10
-    pad[2] = int(raw2/70)-10
+    #pad[0] = int(raw0/37)-21
+    #pad[1] = int(raw1/80)-20
+    #pad[2] = int(raw2/47)-20
 
-#    pad[0] = int(raw0/24)-53
-#    pad[1] = int(raw1/65)-63
-#    pad[2] = int(raw2/25)-51
+    pad[0] = int(raw0/24)-53
+    pad[1] = int(raw1/65)-55
+    pad[2] = int(raw2/25)-51
 
     if pad[0] < 0:
         pad[0] = 0
@@ -63,6 +63,8 @@ def getMidiInput():
             print(str(pad[0]) + "\t" + str(pad[1]) + "\t" + str(pad[2]))
             #print("-----------")
 
+    time.sleep(0.05)
+
     return pad
 
 
@@ -71,6 +73,7 @@ pads = [60,62,64]              # Keeps the pad midi code #s
 padCount = 0
 while (padCount < 3):
     pad = [0, 0, 0]
+    enterKey = 3
 
     print("Tap pad 0 to play current note, pad 2 to up the note, pad 1 to down the note. Hit hard (velocity 127) to save each pad...")
     key = 60
@@ -92,11 +95,35 @@ while (padCount < 3):
             if key >127:
                 key = 127
 
+            if pad[0]:
+                enterKey -= 1
+                if enterKey < 1:
+                    pad[0] = 127
+                    enterKey = 3
+
             print("key = " + str(key))
 #
-            midi.send([NoteOn(key, 50)])
-            midi.send([NoteOff(key, 50)])
-        time.sleep(0.1)
+            midi.send([NoteOn(key, 100)])
+            midi.send([NoteOff(key, 100)])
+        #time.sleep(0.1)
+
+    time.sleep(0.5)
+    midi.send([NoteOn(key, 100)])   # Dah
+    time.sleep(0.5)
+    midi.send([NoteOff(key, 100)])
+    midi.send([NoteOn(key, 100)])   # Di
+    time.sleep(0.25)
+    midi.send([NoteOff(key, 100)])
+    midi.send([NoteOn(key, 100)])   # Di
+    time.sleep(0.25)
+    midi.send([NoteOff(key, 100)])
+    midi.send([NoteOn(key, 100)])   # Dah
+    time.sleep(0.5)
+    midi.send([NoteOff(key, 100)])
+    midi.send([NoteOn(key, 100)])   # Dah
+    time.sleep(0.5)
+    midi.send([NoteOff(key, 100)])
+
 
     pads[padCount] = key
     print("Saved key " + str(padCount))
