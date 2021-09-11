8c1c39a60f3f4aff0f7a924c5558322f416ad5a0
craigy90 <craigy90@gmail.com>
2021-09-11 20:58:06 +1200

    Note adjust basically working - cal. problems still

--- code.py
+++ code.py
@@ -18,85 +18,112 @@ adc0 = analogio.AnalogIn(board.A0)
 adc1 = analogio.AnalogIn(board.A1)
 adc2 = analogio.AnalogIn(board.A2)
 
-stop = 0
-key = 20
-while (stop == 0):
-    print("Enter 'a' to up the note, 'z' to down the note 'q' to exit ...")
-    keyB = input()
-    print(stop)
-    if keyB == 'a':
-        key += 1
-        print('up to ' + str(key))
-    if keyB == 'z':
-        key -= 1
-        print('down to ' + str(key))
-    if keyB == 'q':
-        stop = 1
-    if key < 1:
-        key = 1
-    if key >127:
-        key = 127
-
-    midi.send([NoteOn(key, 127)])
-    #print("pad2")
-    #time.sleep(0.01)
-    midi.send([NoteOff(key, 127)])
-    #time.sleep(1)
-
-while True:
-
+def getMidiInput():
+    pad = [0, 0, 0]
     raw0 = adc0.value
     raw1 = adc1.value
     raw2 = adc2.value
 
-    pad0 = int(raw0/24)-53
-    pad1 = int(raw1/65)-63
-    pad2 = int(raw2/25)-51
+    pad[0] = int(raw0/60)-10
+    pad[1] = int(raw1/120)-10
+    pad[2] = int(raw2/70)-10
 
-    if pad0 < 0:
-        pad0 = 0
-    if pad0 > 127:
-        pad0 = 127
+#    pad[0] = int(raw0/24)-53
+#    pad[1] = int(raw1/65)-63
+#    pad[2] = int(raw2/25)-51
 
-    if pad1 < 0:
-        pad1 = 0
-    if pad1 > 127:
-        pad1 = 127
+    if pad[0] < 0:
+        pad[0] = 0
+    if pad[0] > 127:
+        pad[0] = 127
 
-    if pad2 < 0:
-        pad2 = 0
-    if pad2 > 127:
-        pad2 = 127
+    if pad[1] < 0:
+        pad[1] = 0
+    if pad[1] > 127:
+        pad[1] = 127
 
-    if pad0 or pad1 or pad2:
-        if pad1 > pad0 and pad1 > pad2:
-            pad0 = 0
-            pad2 = 0
-        elif pad2 > pad1 and pad2 > pad0:
-            pad1 = 0
-            pad0 = 0
+    if pad[2] < 0:
+        pad[2] = 0
+    if pad[2] > 127:
+        pad[2] = 127
+
+    if pad[0] or pad[1] or pad[2]:
+        if pad[1] > pad[0] and pad[1] > pad[2]:
+            pad[0] = 0
+            pad[2] = 0
+        elif pad[2] > pad[1] and pad[2] > pad[0]:
+            pad[1] = 0
+            pad[0] = 0
         else:
-            pad2 = 0
-            pad1 = 0
-        #print(pad0, "\t", pad1, "\t", pad2)
+            pad[2] = 0
+            pad[1] = 0
+
+    for note in pad:
+        if note:
+            print(str(pad[0]) + "\t" + str(pad[1]) + "\t" + str(pad[2]))
+            #print("-----------")
+
+    return pad
+
+
+                            # Sound setup routine...
+pads = [60,62,64]              # Keeps the pad midi code #s
+padCount = 0
+while (padCount < 3):
+    pad = [0, 0, 0]
+
+    print("Tap pad 0 to play current note, pad 2 to up the note, pad 1 to down the note. Hit hard (velocity 127) to save each pad...")
+    key = 60
+    while pad[0] < 127 and pad[1] < 127 and pad[2] < 127:
+
+        pad = getMidiInput()
+
+        if pad[0] or pad[1] or pad[2]:
+
+            if pad[2]:
+                key += 1
+                print('up to ' + str(key))
+            if pad[1]:
+                key -= 1
+                print('down to ' + str(key))
+
+            if key < 1:
+                key = 1
+            if key >127:
+                key = 127
+
+            print("key = " + str(key))
+#
+            midi.send([NoteOn(key, 50)])
+            midi.send([NoteOff(key, 50)])
+        time.sleep(0.1)
+
+    pads[padCount] = key
+    print("Saved key " + str(padCount))
+    padCount += 1
+
+
+while True:
+
+    pad0, pad1, pad2 = getMidiInput()
 
     if pad0:
-        midi.send([NoteOn(62, pad0)])
+        midi.send([NoteOn(pads[0], pad0)])
         #print("pad0")
         #time.sleep(0.01)
-        midi.send([NoteOff(62, pad0)])
+        midi.send([NoteOff(pads[0], pad0)])
 
     if pad1:
-        midi.send([NoteOn(60, pad1)])
+        midi.send([NoteOn(pads[1], pad1)])
         #print("pad1")
         #time.sleep(0.01)
-        midi.send([NoteOff(60, pad1)])
+        midi.send([NoteOff(pads[1], pad1)])
 
     if pad2:
-        midi.send([NoteOn(65, pad2)])
+        midi.send([NoteOn(pads[2], pad2)])
         #print("pad2")
         #time.sleep(0.01)
-        midi.send([NoteOff(65, pad2)])
+        midi.send([NoteOff(pads[2], pad2)])
 
 
     #time.sleep(0.01)
